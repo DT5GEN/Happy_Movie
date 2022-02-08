c61c05993d624b1eccbd689a98b96b19e9d577a5
DT5Gen <marinovod@gmail.com>
2022-02-09 01:46:15 +0300

    start NAVibar v0.2.3

--- app/src/main/java/com/example/happymovie/view/MainFragment.kt
+++ app/src/main/java/com/example/happymovie/view/MainFragment.kt
@@ -21,7 +21,7 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_main_viewpager, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/happymovie/view/ViewPagerItems.kt
@@ -0,0 +1,44 @@
+package com.example.happymovie.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.happymovie.R
+import com.example.happymovie.model.TVMovies
+import com.example.happymovie.viewmodel.MainViewModel
+
+class ViewPagerItems : Fragment () {
+
+//
+//    fun newInstance(data: TVMovies): ViewPagerItems {
+//        val fragment = ViewPagerItems()
+//        val args = Bundle()
+//        args.putString("title", data.title)
+//        args.putDouble("vote_average", data.vote_average)
+//        args.putString("poster_path", data.poster_path)
+//        args.putString("backdrop_path", data.backdrop_path)
+//        args.putString("original_lang", data.original_lang)
+//        args.putInt("vote_count", data.vote_count)
+//        args.putString("overview", data.overview)
+//        fragment.arguments = args
+//        return fragment
+//
+//}
+//    private lateinit var viewModel: MainViewModel
+//
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//
+//        val view = inflater.inflate(R.layout.fragment_view_pager_items, container, false)
+//      //  val posterImageView = binding.po
+//
+//        return view
+//    }
+
+}
