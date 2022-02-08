2540d0d7eed04ca06f44762a777d22d8ee9f4444
DT5Gen <marinovod@gmail.com>
2022-02-09 00:08:58 +0300

    start NAVibar

--- app/src/main/java/com/example/happymovie/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/happymovie/viewmodel/MainViewModel.kt
@@ -1,7 +1,29 @@
 package com.example.happymovie.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.happymovie.model.Repository
+import com.example.happymovie.model.RepositoryImpl
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel  (
+
+    private val liveDataToObserve : MutableLiveData<AppState> = MutableLiveData(),
+    private val repositoryImpl: Repository = RepositoryImpl())
+    : ViewModel()
+{
+
+    fun getLiveData() = liveDataToObserve
+
+    fun getMoviesFromLocalSource() = getDataFromLocalSource()
+
+    fun getMoviesFromRemoteSource() = getDataFromLocalSource()
+
+
+    private fun getDataFromLocalSource() {
+        liveDataToObserve.value = AppState.Loading
+        Thread {
+            Thread.sleep(2000)
+            liveDataToObserve.postValue(AppState.Success(repositoryImpl.getMoviesFromServer()))
+        }.start()
+    }
 }
